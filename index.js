d5d950c3a402622036bb781c840327d4c8dcce4a
rakeshhatzade11 <hatzaderakesh1@gmail.com>
2024-04-13 17:38:56 +0530

    get APIs learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Starting server");
+import express from 'express';
+
+const app =express();
+
+const PORT = 5000;
+
+app.get ("/burger" , (req ,res)=>{
+
+    res.json({
+        name:"veg Burger" ,
+        price:50
+    })
+})
+app.get ("/pizza" , (req , res)=>{
+
+    res.json({
+        name:"veg pizza" ,
+        price :100
+    })
+});
+
+app.get ("/pasta" , (req , res)=>{
+ res.json({
+    name:"veg pasta" ,
+    price : 150 
+ })   
+});
+
+app.listen(PORT,()=>{
+console.log('Server is running on port 5000');
+})
